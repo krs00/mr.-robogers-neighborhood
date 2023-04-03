3d35790da646fe176431061d3a77d39ddd639bdc
Kymani Stephens <kymanist00@gmail.com>
2023-04-03 00:04:10 -0700

    WiP coming back for update function

--- js/script.js
+++ js/script.js
@@ -33,17 +33,20 @@ const output = document.getElementById("output")
 function handleNum(e){
     // STOP PAGE FROM REFRESHING
     e.preventDefault();
-    // GET USER INPUT
-    const userInput = document.getElementById("input").value
-    // GENERATE ARRAY CONTAINING WORDS OF USERS INPUTTED NUM
-    let displayArr = beepBoop(userInput)
-    // JOIN ALL ELEMENTS TOGETHER INTO SINGLE STRING
-    const displayString = displayArr.join(", ")
-    // MAKE PARAGRAPH ELEMENT TO APPEND TO PAGE
-    const text = document.createElement('p')
-    text.innerHTML = displayString
+     // GET USER INPUT
+     const userInput = document.getElementById("input").value
+     // GENERATE ARRAY CONTAINING WORDS OF USERS INPUTTED NUM
+     let displayArr = beepBoop(userInput)
+     // JOIN ALL ELEMENTS TOGETHER INTO SINGLE STRING
+     const displayString = displayArr.join(", ")
+     // MAKE PARAGRAPH ELEMENT TO APPEND TO PAGE
+     const text = document.createElement('p')
+     text.innerHTML = displayString
 
-    output.append(text)
+     output.append(text) 
+     
+ 
+     
 }
 
 
