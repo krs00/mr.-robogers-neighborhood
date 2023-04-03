d4aaf0d58fbd36b988a53172d6661109bad72ea6
Kymani Stephens <kymanist00@gmail.com>
2023-04-02 17:12:22 -0700

    update beepBoop function to past final hierarchy test

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ function beepBoop(number) {
         
         let num = i.toString() 
         
-        if (num.includes("1") === true) {
-            arrayBoop.push("Beep!") 
+        if (num.includes("3") === true) {
+            arrayBoop.push("Won't you be my neighbor?") 
         }  else if (num.includes("2") === true) { 
-            arrayBoop.push("Boop!")
-        } else if (num.includes("3") === true) {
-            arrayBoop.push("Won't you be my neighbor?")
+            arrayBoop.push("Boop!") 
+        } else if (num.includes("1") === true) {
+            arrayBoop.push("Beep!")
         } else {
             arrayBoop.push(parseInt(num)) 
         }
